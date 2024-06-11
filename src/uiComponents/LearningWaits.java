3103d39b378befe86bf482715b60b70d8332a59b
unknown <mandyatwork24@gmail.com>
2024-06-11 19:46:38 +0530

    waits

--- src/uiComponents/LearningWaits.java
+++ src/uiComponents/LearningWaits.java
@@ -4,7 +4,10 @@ import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class LearningWaits {
 
@@ -19,9 +22,8 @@ public class LearningWaits {
 		 * 2. wait until defined time limit
 		 * 3. if element displayed before time limit it will not wait unlike Thread.sleep()
 		 */
-		
-		
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		
 		driver.manage().window().maximize();
 		driver.get("https://selenium.qabible.in/dynamic-load.php");
 		driver.findElement(By.cssSelector("#save")).click();
@@ -29,8 +31,26 @@ public class LearningWaits {
 		String name=driver.findElement(By.cssSelector(".card-title")).getText();
 		System.out.println(name);
 		
-
+		driver.findElement(By.id("progress-bars")).click();
+		driver.findElement(By.id("downloadButton")).click();
 		
+		WebElement widgetText= driver.findElement(By.cssSelector(".progress-label"));
+		System.out.println(widgetText.getText());
+		
+		/**
+		 * Explicit wait
+		 * defined for explicit behavior of an element
+		 * 
+		 */
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(8));
+		wait.until(ExpectedConditions.textToBePresentInElement(widgetText, "Complete!"));
+		
+		WebElement btn=driver.findElement(By.cssSelector(".ui-dialog-buttonset button"));
+		
+		System.out.println(driver.findElement(By.cssSelector(".progress-label")).getText());
+		
+        System.out.println(btn.getText());
+        btn.click();
 		
 		driver.quit();
 		
---
+++ src/uiComponents/UploadFile.java
@@ -0,0 +1,48 @@
+package uiComponents;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class UploadFile {
+
+	public static void main(String[] args) throws AWTException, InterruptedException {
+
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.ilovepdf.com/pdf_to_word");
+		driver.findElement(By.cssSelector("#pickfiles")).click();
+		
+		String fileToUpload= System.getProperty("user.dir")+"\\fileName.pdf";
+		uploadFile(fileToUpload);
+		
+	
+		driver.quit();
+	}
+	
+	public static void uploadFile(String fileToUpload) throws AWTException, InterruptedException {
+		StringSelection ss = new StringSelection(fileToUpload);
+	    Toolkit.getDefaultToolkit().getSystemClipboard().setContents(ss, null);
+
+	    Thread.sleep(1000);
+	     //imitate mouse events like ENTER, CTRL+C, CTRL+V
+	     Robot robot = new Robot();
+	     robot.delay(250);
+	     robot.keyPress(KeyEvent.VK_ENTER);
+	     robot.keyRelease(KeyEvent.VK_ENTER);
+	     robot.keyPress(KeyEvent.VK_CONTROL);
+	     robot.keyPress(KeyEvent.VK_V);
+	     robot.keyRelease(KeyEvent.VK_V);
+	     robot.keyRelease(KeyEvent.VK_CONTROL);
+	     robot.keyPress(KeyEvent.VK_ENTER);
+	     robot.delay(90);
+	     robot.keyRelease(KeyEvent.VK_ENTER);
+	}
+
+}
